aa341f49be5ea3fbdd0a803732a315b2204c6f44
Gabriel Zsigmond <gaazsig@gmail.com>
2020-04-20 05:21:02 -0300

    complete unit 6.1 basics of the linux cammand line

---
+++ unit6_the_command_line_interface/unit6.1_history_of_linux_and_the_command_line/src/commands.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+#comment using vim
+echo whoami:
+whoami
+
+echo id:
+id
+
+echo logname:
+logname
+
+echo hostname:
+hostname
+
+echo uname:
+uname
+
+echo uptime:
+uptime
+
+echo cal:
+cal
+
+echo date:
+date
+
+echo date +"%T":
+date +"%T"
+
+echo date +"%A %d %B %Y":
+date +"%A %d %B %Y"
+
